a6821069f703fb36c8b0342a8a1e51bc212d4e47
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2010-12-17 14:30:16 +0000

    Make mouse tracking work in anipanel & allow drag re-arrangement of nodes
    anipanel:
    
    * Fix test of mouse position - it should have been using the transformed
      location, of the position of the mouse in model co-ordinates.
    * Add support for dragging nodes to new locations, using a tri-state
      and the new setPosition method for positionable nodes.
    * Draw the Id instead of the size on mouse-over

--- src/agarnet/anipanel.java
+++ src/agarnet/anipanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.NoninvertibleTransformException;
@@ -35,6 +36,8 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
   private boolean textlabels = true;
   private int mouse_x;
   private int mouse_y;
+  private Point mouse_pressed_p;
+  private H host_dragging;
   
   public anipanel (Simulation<I,H> s) {
     this.s = s;
@@ -43,6 +46,7 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
     
     //this.setSize (1000, 600);
     this.setBackground (new Color (0,0,0));
+    
     this.addMouseListener (new java.awt.event.MouseAdapter () {
       @Override
       public void mouseExited (MouseEvent e) {
@@ -50,18 +54,41 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
       }
 
       @Override
-      public void mouseMoved (MouseEvent e) {
-        mouse_x = e.getX ();
-        mouse_y = e.getY ();
-      }
-
-      @Override
       public void mouseEntered (MouseEvent e) {
         mouse_in_panel = true;
       }
-        
+
+      @Override
+      public void mousePressed (MouseEvent e) {
+        super.mousePressed (e);
+        mouse_pressed_p = e.getPoint ();
       }
-    );
+
+      @Override
+      public void mouseReleased (MouseEvent e) {
+        super.mouseReleased (e);
+        host_dragging = null;
+        mouse_pressed_p = null;
+      }
+    });
+    
+    this.addMouseMotionListener (new java.awt.event.MouseMotionAdapter () {
+      public void mouseMoved (MouseEvent e) {
+        super.mouseMoved (e);
+        mouse_x = e.getX ();
+        mouse_y = e.getY ();
+        anipanel.this.repaint ();
+      }
+
+      @Override
+      public void mouseDragged (MouseEvent e) {
+        super.mouseDragged (e);
+        mouse_x = e.getX ();
+        mouse_y = e.getY ();
+        anipanel.this.repaint ();
+      }
+      
+    });
   }
   
   public double noderadius () {
@@ -152,10 +179,18 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
         }
       }
       
-      Point2D.Double mousep = new Point2D.Double ();
+      Point2D.Double mousep = null;
+      Point2D.Double mouse_press_model = null;
       
-      if (mouse_in_panel)
+      if (mouse_in_panel) {
+        mousep = new Point2D.Double ();
         t.inverseTransform (new Point2D.Double (mouse_x, mouse_y), mousep);
+        
+        if (mouse_pressed_p != null) {
+          mouse_press_model = new Point2D.Double ();
+          t.inverseTransform (mouse_pressed_p, mouse_press_model);
+        }
+      }
       
       for (H p : s.network) {
         boolean show_tip = false;
@@ -165,11 +200,21 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
         Point2D pos = p.getPosition ();
         
         if (mouse_in_panel) {
-          if (mouse_x > pos.getX () - noderadius &&
-              mouse_x < pos.getX () + noderadius &&
-              mouse_y > pos.getY () - noderadius &&
-              mouse_y < pos.getY () + noderadius)
+          if (mousep.x > pos.getX () - noderadius &&
+              mousep.x < pos.getX () + noderadius &&
+              mousep.y > pos.getY () - noderadius &&
+              mousep.y < pos.getY () + noderadius) {
             show_tip = true;
+            
+            if (mouse_press_model != null) {
+              mouse_pressed_p = null;
+              host_dragging = p;
+            }
+          }
+          if (host_dragging != null) {
+            host_dragging.setPosition (new Vector2D (mousep));
+            pos = p.getPosition ();
+          }
         }
         
         g.fillOval ((int)(pos.getX () - noderadius),
@@ -188,9 +233,9 @@ public class anipanel<I, H extends AnimatableHost<Long,H>>
                       (int)(pos.getY () + noderadius), 
                       (int) noderadius * 2, (int)noderadius * 2);
           g.setColor (Color.red);
-          g.drawString (Integer.toString (Math.round (p.getSize ())),
-                        (int)(pos.getX () - noderadius * 0.75),
-                        (int)(pos.getY () + noderadius * 0.75));
+          g.drawString (Integer.toString (Math.round (p.getId ())),
+                        (int)(pos.getX () - (noderadius * 0.75)),
+                        (int)(pos.getY () + (noderadius * 2)));
         }
         
       }
