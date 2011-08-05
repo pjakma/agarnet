deb8fa7fa1ae6d8b1d1bd42509e5e1770a88c45b
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2011-08-05 14:58:26 +0100

    Provide the obvious Coloured implementations in AnimatableHost, and allow set.
    * framework/Coloured: Allow colour also to be set.
    * AnimatableHost: provide the obvious, trivial default implementation.

--- src/agarnet/framework/Coloured.java
+++ src/agarnet/framework/Coloured.java
@@ -4,4 +4,5 @@ import java.awt.Color;
 
 public interface Coloured {
   public Color colour ();
+  public void colour (Color c);
 }
--- src/agarnet/protocols/host/AnimatableHost.java
+++ src/agarnet/protocols/host/AnimatableHost.java
@@ -4,6 +4,8 @@ import agarnet.framework.Coloured;
 import agarnet.framework.Simulation;
 import agarnet.protocols.protocol;
 
+import java.awt.Color;
+
 public abstract class AnimatableHost<I,N> extends PositionableHost<I,N>
                                  implements Coloured {
   protected AnimatableHost () {}
@@ -11,5 +13,14 @@ public abstract class AnimatableHost<I,N> extends PositionableHost<I,N>
                            boolean movable, 
                            protocol<I> [] protocols) {
     super (sim, movable, protocols);
+  }
+  
+  private Color colour = new Color (255, 255, 255);
+  public Color colour () {
+    /* Color appears to be immutable, so this should always be fine */
+    return colour;
+  }
+  public void colour (Color c) {
+    colour = c;
   }
 }
