83e1d03dfce50ee9bfb1f759e162c9a80f99d6e8
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2013-04-15 15:29:58 +0100

    kcore: Make calc_kbound a little more efficient, and trim down debug
    * kcore.java: (link_update) note that generation update isn't needed for
      removes of neighbours - algorithm just ocntinues downward.
      Remove or otherwise trim down lots of debug.
      (calc_kbound) Remember the total so far across each check of new, lower
      kbound, and so get rid of the inner loop calculating the total afresh each
      time.
      (up) Check neighbour exists
      (toString) fairly hefty toString for aiding debug.

--- src/kcoresim/kcore.java
+++ src/kcoresim/kcore.java
@@ -88,7 +88,7 @@ public class kcore<N> extends AbstractProtocol<Long> {
   public void link_update () {
     Set<Long> newc = sim.connected (this.selfId);
     
-    debug.println ("link_update...");
+    debug.printf ("%d: link_update...\n", selfId);
     
     if (newc != null) {
       /* Find new neighbours */
@@ -105,6 +105,7 @@ public class kcore<N> extends AbstractProtocol<Long> {
         Long neigh = it.next ();
         if (!newc.contains (neigh)) {
           it.remove ();
+          /* no generation update is required for removes */
           neighbours.remove (neigh);
         }
       }
@@ -118,7 +119,7 @@ public class kcore<N> extends AbstractProtocol<Long> {
   }
   
   private void broadcast (int kbound) {
-    debug.printf ("%d\n", kbound);
+    debug.printf ("%d: %d\n", selfId, kbound);
     
     for (Long neigh : connected)
       send (neigh, get_default_new_nmsg ());
@@ -130,9 +131,7 @@ public class kcore<N> extends AbstractProtocol<Long> {
     int kbound = connected.size ();
     int [] seen = new int [connected.size () + 1];
     int highestseen = 0;
-    
-    debug.printf ("neighb vals: %s\n", neighbours.values ());
-    
+        
     for (neighbour_msg nmsg : neighbours.values ()) {
       int neigh_bound = (nmsg.gen == generation) ? nmsg.kbound
                                                  : connected.size ();
@@ -148,18 +147,13 @@ public class kcore<N> extends AbstractProtocol<Long> {
       for (int i = 0; i < seen.length; i++)
         sbseen.append (" " + seen[i]);
       
-      debug.printf ("seen: %s\n", sbseen);
+      debug.printf ("%d: seen: %s\n", selfId, sbseen);
     }
     
-    for (int i = highestseen; i >= 0; i--) {
-      int tot = 0;
-      
+    for (int i = highestseen, tot = 0; i >= 0; i--) {
       kbound = i;
       
-      for (int j = i; j < seen.length; j++)
-        tot += seen[j];
-      
-      debug.printf ("i %d, tot %d\n", i, tot);
+      tot += seen[i];
       
       if (tot < i)
         continue;
@@ -167,7 +161,7 @@ public class kcore<N> extends AbstractProtocol<Long> {
       break;
     }
     
-    debug.printf ("result %d\n", kbound);
+    debug.printf ("%d: result %d\n", selfId, kbound);
     
     return kbound;
   }
@@ -175,22 +169,22 @@ public class kcore<N> extends AbstractProtocol<Long> {
   private boolean kbound () {
     int origkbound = kbound;
     
-    debug.printf ("start, orig %d\n", origkbound);
+    debug.printf ("%d: start, orig %d\n", selfId, origkbound);
     kbound = calc_kbound ();
-    debug.printf ("got %d\n", kbound);
+    debug.printf ("%d: got %d\n", selfId, kbound);
     
     return (origkbound != kbound);
   }
   
   @Override
   public void up(Long src, byte[] data) {
-    debug.printf ("peer %s: receive msg from %s\n", this, src);
+    debug.printf ("%s: receive msg from %s\n", selfId, src);
     neighbour_msg m = null;
     
     try {
       m = marshall.deserialise (m, data);
     } catch (Exception e) {
-      debug.println (debug.levels.ERROR, "Unhandled message!");
+      debug.printf (debug.levels.ERROR, "%d: Unhandled message!\n", selfId);
       return;
     }
         
@@ -201,15 +195,46 @@ public class kcore<N> extends AbstractProtocol<Long> {
       return;
     }
     
+    if (neighbours.get (m.srcid) == null) {
+      debug.printf (debug.levels.ERROR, "%d: Unknown neighour %d!\n", selfId, src);
+    }
+    
     stats_inc (stat.recvd);
     
     generation_check (m);
     
     neighbours.put (src, m);
     
-    debug.printf ("Received %s from %d\n", m, src);
+    debug.printf ("%s: Received %s from %d\n", selfId, m, src);
     
     if (kbound () || genupdated)
       broadcast (kbound);
+  }
+  
+  public String toString () {
+    StringBuilder sb = new StringBuilder ();
+
+    sb.append (super.toString ());
+    sb.append ("\nkcore: kb/gen/deg: (" + this.kbound);
+    sb.append ("," + this.generation);
+    sb.append ("," + this.connected.size ());
+    sb.append ("\nconnected:\n" + this.connected);
+    sb.append ("\nneighbours:");
+    for (Long neigh : neighbours.keySet ()) {
+      neighbour_msg m = neighbours.get (neigh);
+      if (m.kbound >= this.kbound) {
+        simhost remote = (simhost) sim.id2node (m.srcid );
+        sb.append (String.format ("\n  msg from %d (degree %d): ",
+                   m.srcid, remote.degree ()));
+        sb.append (String.format ("got/actual (%d,%d) (%d,%d)%s",
+                                  m.gen, m.kbound,
+                                  remote.kgen (), remote.kbound (),
+                                  (m.gen != remote.kgen ()
+                                   || m.kbound != remote.kbound ()) ? "!" : ""
+                                  ));
+      }
+    }
+    sb.append ('\n');
+    return sb.toString ();
   }
 }
