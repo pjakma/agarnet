c5209fd639392c9d9865bd3d4efda8ed9c70f7c9
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2013-05-10 17:24:15 +0100

    run.sh: Add profile and GC options for JVM

--- run.sh
+++ run.sh
@@ -4,5 +4,10 @@ CPATH="$PROJECTHOME/MultiGraph/build/classes"
 CPATH="$CPATH:$PROJECTHOME/agarnet/build/jar/agarnet.jar"
 CPATH="$CPATH:/usr/share/java/gnu.getopt.jar"
 CPATH="$CPATH:$PROJECTHOME/kcoresim/build/jar/kcoresim.jar"
+#PROFOPTS="-Xprof"
+#PROFOPTS="-Xrunhprof"
+#PROFOPTS="-javaagent:/home/paul/Downloads/shiftone-jrat.jar"
+GCOPTS="-XX:+UseConcMarkSweepGC"
 
-java -server -classpath $CPATH kcoresim.kcoresim $@
+java -Xverify:none ${PROFOPTS} ${GCOPTS} \
+	-server -classpath $CPATH kcoresim.kcoresim $@
