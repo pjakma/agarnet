2d85d28001f86fda78c99a00bad170d86f17472e
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2013-05-13 14:36:46 +0100

    block_descriptor: remove unneeded imports

--- src/basicp2psim/protocols/peer/data/block_descriptor.java
+++ src/basicp2psim/protocols/peer/data/block_descriptor.java
@@ -1,10 +1,5 @@
 package basicp2psim.protocols.peer.data;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class block_descriptor implements Serializable {
