b8022934318082caafc2ac577706d818d7c032a0
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2014-04-15 16:46:41 +0100

    run.sh: Use the MultiGraph jar rather than build dir for classes

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 PROJECTHOME="/home/paul/code"
 
-CPATH="$PROJECTHOME/MultiGraph/build/classes"
+CPATH="$PROJECTHOME/MultiGraph/build/jar/MultiGraph.jar"
 CPATH="$CPATH:$PROJECTHOME/agarnet/build/jar/agarnet.jar"
 CPATH="$CPATH:/usr/share/java/gnu.getopt.jar"
 CPATH="$CPATH:$PROJECTHOME/kcoresim/build/jar/kcoresim.jar"
