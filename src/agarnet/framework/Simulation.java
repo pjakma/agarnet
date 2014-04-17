1b64f59bf999b28fe113dfae667053ff293e5358
Paul Jakma <paul@jamaica.dcs.gla.ac.uk>
2014-04-17 13:30:35 +0100

    Simulation: Fix whitespace/indentation

--- src/agarnet/framework/Simulation.java
+++ src/agarnet/framework/Simulation.java
@@ -15,46 +15,46 @@ import agarnet.link.link;
  * @param <H> The type used for Hosts in the Simulation
  */
 public abstract class Simulation<I,H> extends Observable {
-	public final Graph<H,link<H>> network;
-	public final Dimension model_size;
+  public final Graph<H,link<H>> network;
+  public final Dimension model_size;
+  
+  public Simulation (Graph<H,link<H>> g, Dimension d) {
+    this.model_size = d;
+    this.network = g;
+  }
 	
-	public Simulation (Graph<H,link<H>> g, Dimension d) {
-	  this.model_size = d;
-	  this.network = g;
-	}
+  /**
+   * @param The node to query the simulation about
+   * @return The set of nodes which are connected to the given node.
+   */
+  abstract public Set<I> connected (I node);
 	
-	/**
-	 * @param The node to query the simulation about
-	 * @return The set of nodes which are connected to the given node.
+  /**
+   * Transmit the packet from one node to the other, I.e. link-layer. 
+   * This should therefore only be called from the bottom of the host's
+   * protocol stack, usually.
+   * @param from Id of the sending node
+   * @param to  Id of the node to send to
+   * @param data Opaque data
+   */
+  abstract public boolean tx (I from, I to, byte [] data);
+  
+  /**
+   * Map from the H-typed Host object from which the network Graph is made
+   * to the I-typed IDs the Simulation generally uses for nodes.
+   * 
+   * Using these methods is a layering violation and generally discouraged. 
+   * However there may be certain, very special cases where this is
+   * justified.
+   *
+   * @param node The node object to retrieve the I typed Id for
+   * @return The Id of the given node
 	 */
-	abstract public Set<I> connected (I node);
-	
-	/**
-	 * Transmit the packet from one node to the other, I.e. link-layer. 
-	 * This should therefore only be called from the bottom of the host's
-	 * protocol stack, usually.
-	 * @param from Id of the sending node
-         * @param to  Id of the node to send to
-         * @param data Opaque data
-	 */
-	abstract public boolean tx (I from, I to, byte [] data);
-	
-	/**
-	 * Map from the H-typed Host object from which the network Graph is made
-	 * to the I-typed IDs the Simulation generally uses for nodes.
-	 * 
-	 * Using these methods is a layering violation and generally discouraged. 
-	 * However there may be certain, very special cases where this is
-         * justified.
-         *
-         * @param node The node object to retrieve the I typed Id for
-         * @return The Id of the given node
-	 */
-        abstract public I node2id(H node);
-        /**
+   abstract public I node2id(H node);
+   /**
 	 * retrieve the appropriate Host for the given ID
-         * @param node the node Id to lookup
-         * @return the Host object for the given Id
+   * @param node the node Id to lookup
+   * @return the Host object for the given Id
 	 */
-        abstract public H id2node (I node);
+   abstract public H id2node (I node);
 }
