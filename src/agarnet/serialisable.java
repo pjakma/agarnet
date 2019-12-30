ed34b120a43e1b0ac84753c89e672327e80f2547
Paul Jakma <paul@jakma.org>
2019-12-30 16:50:34 +0000

    agarnet.serialisable: explain reason for this interface

--- src/agarnet/serialisable.java
+++ src/agarnet/serialisable.java
@@ -19,6 +19,28 @@ package agarnet;
 
 import java.io.IOException;
 
+/**
+ * Marker interface for a serialisation interface of objects.
+ *
+ * Similarish in spirit to the standard Java Serializable interface, however
+ * this allows for a more direct and lower-overhead implementation,
+ * serialising straight to byte[] and not having to rely on the standard
+ * Serializable infrastructure - which has a degree of overhead, that is
+ * noticeable in performance at scale.
+ *
+ * Given the near-duplication of name with Serializable, this interface
+ * should generally by referred to by the fully qualified
+ * 'agarnet.serialisable' and not imported.
+ *
+ * This interface expects that an implementing class T also implements a
+ * static method:
+ * 
+ * 	static T deserialise (byte [])
+ *
+ * There is no reasonable way to specify this requirement in Java, in a
+ * well-typed way, that I could find, other than with tricks with reflection
+ * - which would defeat the performance motivation for this interface.
+ */
 public interface serialisable {
   public byte [] serialise () throws IOException;
 }
