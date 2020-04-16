9cd3b31e3e61d79eaa5b8a6f512f21ce3b8c21c2
Paul Jakma <pjakma@fb.com>
2020-04-16 17:22:19 +0100

    agarnet/run.sh: shell check lint

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # run as: run.sh {name of main class} <arguments>
 #
 # e.g.
@@ -7,19 +8,19 @@
 
 # If dependencies like MultiGraph are sibling directories
 #PROJECTHOME=$(realpath ..)
-PROJECTHOME=$(realpath $(dirname $0)/..)
+PROJECTHOME="$(realpath $(dirname "$0")/..)"
 
 CPATH="$PROJECTHOME/MultiGraph/build/jar/MultiGraph.jar"
 CPATH="$CPATH:$PROJECTHOME/agarnet/build/jar/agarnet.jar"
 CPATH="$CPATH:/usr/share/java/gnu.getopt.jar"
 #CPATH="$CPATH:$PROJECTHOME/kcoresim/build/jar/kcoresim.jar"
-#PROFOPTS="-Xprof"
-#PROFOPTS="-Xrunhprof"
-GCOPTS="-XX:+UseConcMarkSweepGC"
-JAVAOPTS="${JAVAOPTS} -XX:+UseLargePages "
-JAVAOPTS="${JAVAOPTS} -Xverify:none"
+#PROFOPTS="(-Xprof)"
+#PROFOPTS="(-Xrunhprof)"
+GCOPTS=(-XX:+UseConcMarkSweepGC)
+JAVAOPTS=("${JAVAOPTS[@]}" -XX:+UseLargePages)
+JAVAOPTS=("${JAVAOPTS[@]}" -Xverify:none)
 #  -Dsun.java2d.opengl=True needed for Java2D to not be super-slow,
 # with anti-aliasing on Linux.
-JAVAOPTS="${JAVAOPTS} -DuseAA=true -Dsun.java2d.opengl=True"
-java ${JAVAOPTS} ${PROFOPTS} ${GCOPTS} \
-        -server -classpath $CPATH $@
+JAVAOPTS=("${JAVAOPTS[@]}" '-DuseAA=true' '-Dsun.java2d.opengl=True')
+java "${JAVAOPTS[@]}" "${PROFOPTS[@]}" "${GCOPTS[@]}" \
+        -server -classpath "$CPATH" "$@"
